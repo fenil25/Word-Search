f56105473052a0b3b2dffdade2c0e8b44048e534
jimitgandhi <jimitgandhi@outlook.com>
2018-03-20 03:29:32 +0530

    Commit 1

--- app/src/main/java/com/fenil/codesprint/MainGame.java
+++ app/src/main/java/com/fenil/codesprint/MainGame.java
@@ -2,8 +2,13 @@ package com.fenil.codesprint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainGame extends AppCompatActivity {
+
+    public void select(View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

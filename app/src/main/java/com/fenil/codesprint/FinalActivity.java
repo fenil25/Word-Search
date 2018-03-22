3e7fc2faad2705dfff45a922f9b8d42e247a8c67
fenil-25 <fenil25@github.com>
2018-03-22 12:50:07 +0530

    Final activity done!

--- app/src/main/java/com/fenil/codesprint/FinalActivity.java
+++ app/src/main/java/com/fenil/codesprint/FinalActivity.java
@@ -3,10 +3,13 @@ package com.fenil.codesprint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FinalActivity extends AppCompatActivity {
     TextView sc;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +19,14 @@ public class FinalActivity extends AppCompatActivity {
         int score = i.getIntExtra("score", 0);
         sc = (TextView) findViewById(R.id.textView2);
         sc.setText(score+"");
+
+        b = (Button) findViewById(R.id.button5);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(FinalActivity.this, MainGame.class);
+                startActivity(a);
+            }
+        });
     }
 }
